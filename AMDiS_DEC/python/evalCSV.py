544c8466320e99573050748d25ac0a2084cb5186
Ingo Nitschke on the Klapprechner <ingo.nitschke@tu-dresden.de>
2014-02-23 15:57:08 +0100

    python and co

--- AMDiS_DEC/python/evalCSV.py
+++ AMDiS_DEC/python/evalCSV.py
@@ -1,10 +1,23 @@
 import csv
+from pylab import *
+#name = ["MaxMaxAngle", "AvMaxAngle"]
+name = ["AvArea", "MinArea","MaxArea"]
+n = len(name)
+last = 20
 with open('../meshStatsSphereDivBy4.csv', 'rb') as f:
-    reader = csv.reader(f)
-    n = 0
-    for col in reader[0]:
-        if (col == "MaxMaxAngle"):
-          break
-        n = n + 1
+    reader = csv.DictReader(f)
+    x = n*[ndarray((0,1),dtype=double)]
+    k = 0
+    for row in reader:
+      for i in arange(n):
+        x[i] = append(x[i], double(row[name[i]]))
+      if k == last: break
+      k = k+1
 
-print n
+
+for i in arange(n):
+  #semilogx(x[i], label=name[i])
+  plot(x[i], label=name[i])
+
+legend()
+show()
