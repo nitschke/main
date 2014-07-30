ae0572b014eeb02457ceb52638f46c7a957314a3
nitschke <ingo.nitschke@tu-dresden.de>
2014-07-30 19:43:14 +0200

    ...

--- AMDiS_DEC/src/sphereGrad.cc
+++ AMDiS_DEC/src/sphereGrad.cc
@@ -61,29 +61,29 @@ int main(int argc, char* argv[])
     PrimalPrimalGradFunctionDEC *rhsOperator = new PrimalPrimalGradFunctionDEC(i, new X(2), sphere.getFeSpace(i));
     sphere.addVectorOperator(rhsOperator, i);
   }
-  // Div(X)
-  SimpleDEC *decOperator = new SimpleDEC(sphere.getFeSpace(3),sphere.getFeSpace(3) );
-  sphere.addMatrixOperator(decOperator, 3, 3);
-  for (int i = 0; i < 3; i++) {
-    PrimalPrimalGradFunctionDEC *rhsOperator = new PrimalPrimalGradFunctionDEC(i, new X(i), sphere.getFeSpace(3));
-    sphere.addVectorOperator(rhsOperator, 3);
-  }
+  //// Div(X)
+  //SimpleDEC *decOperator = new SimpleDEC(sphere.getFeSpace(3),sphere.getFeSpace(3) );
+  //sphere.addMatrixOperator(decOperator, 3, 3);
+  //for (int i = 0; i < 3; i++) {
+  //  PrimalPrimalGradFunctionDEC *rhsOperator = new PrimalPrimalGradFunctionDEC(i, new X(i), sphere.getFeSpace(3));
+  //  sphere.addVectorOperator(rhsOperator, 3);
+  //}
 
-  for (int i = 4; i < 7; i++) {
-    SimpleDEC *Ni = new SimpleDEC(sphere.getFeSpace(i),sphere.getFeSpace(i));
-    sphere.addMatrixOperator(decOperator, i, i);
+  //for (int i = 4; i < 7; i++) {
+  //  SimpleDEC *Ni = new SimpleDEC(sphere.getFeSpace(i),sphere.getFeSpace(i));
+  //  sphere.addMatrixOperator(decOperator, i, i);
 
-    //FunctionDEC *Ndpi = new FunctionDEC(new X(i-4), sphere.getFeSpace(i));
-    DualPrimalNormalDEC *Ndpi = new DualPrimalNormalDEC(i-4, sphere.getFeSpace(i));
-    sphere.addVectorOperator(Ndpi, i);
-  }
-  SimpleDEC *M2K = new SimpleDEC(sphere.getFeSpace(7),sphere.getFeSpace(7) );
-  M2K->setFactor(-1.0);
-  sphere.addMatrixOperator(M2K, 7, 7);
-  for (int i = 4; i < 7; i++) {
-    PrimalPrimalGradDEC *GRDiNi = new PrimalPrimalGradDEC(i-4, sphere.getFeSpace(7),sphere.getFeSpace(i));
-    sphere.addMatrixOperator(GRDiNi, 7, i);
-  }
+  //  //FunctionDEC *Ndpi = new FunctionDEC(new X(i-4), sphere.getFeSpace(i));
+  //  DualPrimalNormalDEC *Ndpi = new DualPrimalNormalDEC(i-4, sphere.getFeSpace(i));
+  //  sphere.addVectorOperator(Ndpi, i);
+  //}
+  //SimpleDEC *M2K = new SimpleDEC(sphere.getFeSpace(7),sphere.getFeSpace(7) );
+  //M2K->setFactor(-1.0);
+  //sphere.addMatrixOperator(M2K, 7, 7);
+  //for (int i = 4; i < 7; i++) {
+  //  PrimalPrimalGradDEC *GRDiNi = new PrimalPrimalGradDEC(i-4, sphere.getFeSpace(7),sphere.getFeSpace(i));
+  //  sphere.addMatrixOperator(GRDiNi, 7, i);
+  //}
   // ===== start adaption loop =====
   adapt->adapt();
 
